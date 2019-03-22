69e89c37a634bb05cb8b239ef6e4070e21d8d3cf
voledemar <voledemar@inbox.ru>
2019-03-23 01:39:37 +0300

    HERE map init

--- app/src/main/java/org/neweralosguys/hackuniversity/MainActivity.java
+++ app/src/main/java/org/neweralosguys/hackuniversity/MainActivity.java
@@ -1,13 +1,58 @@
 package org.neweralosguys.hackuniversity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.SupportMapFragment;
+
+import java.io.File;
+
+public class MainActivity extends FragmentActivity {
+
+    // map embedded in the map fragment
+    private Map map = null;
+
+    // map fragment embedded in this activity
+    private SupportMapFragment mapFragment = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initialize();
+    }
+
+    private void initialize() {
         setContentView(R.layout.activity_main);
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
+        // Set up disk cache path for the map service for this application
+        // It is recommended to use a path under your application folder for storing the disk cache
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(
+                getApplicationContext().getExternalFilesDir(null) + File.separator + ".here-maps",
+                "{YOUR_INTENT_NAME}"); /* ATTENTION! Do not forget to update {YOUR_INTENT_NAME} */
+        if (!success) {
+            Toast.makeText(getApplicationContext(), "Unable to set isolated disk cache path.", Toast.LENGTH_LONG);
+        } else {
+            mapFragment.init(new OnEngineInitListener() {
+                @Override
+                public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+                    if (error == OnEngineInitListener.Error.NONE) {
+                        // retrieve a reference of the map from the map fragment
+                        map = mapFragment.getMap();
+                        // Set the map center to the Vancouver region (no animation)
+                        map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
+                                Map.Animation.NONE);
+                        // Set the zoom level to the average between min and max
+                        map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                    } else {
+                        System.out.println("ERROR: Cannot initialize Map Fragment");
+                    }
+                }
+            });
+        }
     }
 }
