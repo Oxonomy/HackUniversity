3ed3e0893048122aa4919af1bb7c1812999783ea
voledemar <voledemar@inbox.ru>
2019-03-23 14:46:07 +0300

    blablabla

--- app/src/main/java/org/neweralosguys/hackuniversity/MainActivity.java
+++ app/src/main/java/org/neweralosguys/hackuniversity/MainActivity.java
@@ -1,58 +1,22 @@
 package org.neweralosguys.hackuniversity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-import com.here.android.mpa.common.GeoCoordinate;
-import com.here.android.mpa.common.OnEngineInitListener;
-import com.here.android.mpa.mapping.Map;
-import com.here.android.mpa.mapping.SupportMapFragment;
-
-import java.io.File;
-
-public class MainActivity extends FragmentActivity {
-
-    // map embedded in the map fragment
-    private Map map = null;
-
-    // map fragment embedded in this activity
-    private SupportMapFragment mapFragment = null;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initialize();
+        setContentView(R.layout.activity_main);
     }
 
-    private void initialize() {
-        setContentView(R.layout.activity_main);
-        // Search for the map fragment to finish setup by calling init().
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
-        // Set up disk cache path for the map service for this application
-        // It is recommended to use a path under your application folder for storing the disk cache
-        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(
-                getApplicationContext().getExternalFilesDir(null) + File.separator + ".here-maps",
-                "{YOUR_INTENT_NAME}"); /* ATTENTION! Do not forget to update {YOUR_INTENT_NAME} */
-        if (!success) {
-            Toast.makeText(getApplicationContext(), "Unable to set isolated disk cache path.", Toast.LENGTH_LONG);
-        } else {
-            mapFragment.init(new OnEngineInitListener() {
-                @Override
-                public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
-                    if (error == OnEngineInitListener.Error.NONE) {
-                        // retrieve a reference of the map from the map fragment
-                        map = mapFragment.getMap();
-                        // Set the map center to the Vancouver region (no animation)
-                        map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
-                                Map.Animation.NONE);
-                        // Set the zoom level to the average between min and max
-                        map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
-                    } else {
-                        System.out.println("ERROR: Cannot initialize Map Fragment");
-                    }
-                }
-            });
+    public void launchMapActivity(View view) {
+        switch (view.getId()) {
+            case R.id.changeActivityButton:
+                startActivity(new Intent(this, MapActivity.class));
         }
     }
 }
---
+++ app/src/main/java/org/neweralosguys/hackuniversity/MapFragmentView.java
@@ -0,0 +1,207 @@
+package org.neweralosguys.hackuniversity;
+
+import java.io.File;
+import java.util.List;
+
+import com.here.android.mpa.common.GeoBoundingBox;
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.SupportMapFragment;
+import com.here.android.mpa.mapping.MapRoute;
+import com.here.android.mpa.routing.CoreRouter;
+import com.here.android.mpa.routing.RouteOptions;
+import com.here.android.mpa.routing.RoutePlan;
+import com.here.android.mpa.routing.RouteResult;
+import com.here.android.mpa.routing.RouteWaypoint;
+import com.here.android.mpa.routing.Router;
+import com.here.android.mpa.routing.RoutingError;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * This class encapsulates the properties and functionality of the Map view.A route calculation from
+ * HERE Burnaby office to Langley BC is also being handled in this class
+ */
+public class MapFragmentView {
+    private SupportMapFragment m_mapFragment;
+    private Button m_createRouteButton;
+    private AppCompatActivity m_activity;
+    private Map m_map;
+    private MapRoute m_mapRoute;
+
+    public MapFragmentView(AppCompatActivity activity) {
+        m_activity = activity;
+        initMapFragment();
+        /*
+         * We use a button in this example to control the route calculation
+         */
+        initCreateRouteButton();
+    }
+
+    private SupportMapFragment getMapFragment() {
+        return (SupportMapFragment) m_activity.getSupportFragmentManager().findFragmentById(R.id.mapfragment);
+    }
+
+    private void initMapFragment() {
+        /* Locate the mapFragment UI element */
+        m_mapFragment = getMapFragment();
+
+        // Set path of isolated disk cache
+        String diskCacheRoot = Environment.getExternalStorageDirectory().getPath()
+                + File.separator + ".isolated-here-maps";
+        // Retrieve intent name from manifest
+        String intentName = "";
+        try {
+            ApplicationInfo ai = m_activity.getPackageManager().getApplicationInfo(m_activity.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            intentName = bundle.getString("INTENT_NAME");
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(this.getClass().toString(), "Failed to find intent name, NameNotFound: " + e.getMessage());
+        }
+
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(diskCacheRoot, intentName);
+        if (!success) {
+            // Setting the isolated disk cache was not successful, please check if the path is valid and
+            // ensure that it does not match the default location
+            // (getExternalStorageDirectory()/.here-maps).
+            // Also, ensure the provided intent name does not match the default intent name.
+        } else {
+            if (m_mapFragment != null) {
+                /* Initialize the SupportMapFragment, results will be given via the called back. */
+                m_mapFragment.init(error -> {
+                    if (error == OnEngineInitListener.Error.NONE) {
+                        /* get the map object */
+                        m_map = m_mapFragment.getMap();
+
+                        /*
+                         * Set the map center to the 4350 Still Creek Dr Burnaby BC (no animation).
+                         */
+                        m_map.setCenter(new GeoCoordinate(49.259149, -123.008555, 0.0),
+                                Map.Animation.NONE);
+
+                        /* Set the zoom level to the average between min and max zoom level. */
+                        m_map.setZoomLevel((m_map.getMaxZoomLevel() + m_map.getMinZoomLevel()) / 2);
+                    } else {
+                        Toast.makeText(m_activity,
+                                "ERROR: Cannot initialize Map with error " + error,
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+    }
+
+    private void initCreateRouteButton() {
+        m_createRouteButton = m_activity.findViewById(R.id.button);
+
+        m_createRouteButton.setOnClickListener(v -> {
+            /*
+             * Clear map if previous results are still on map,otherwise proceed to creating
+             * route
+             */
+            if (m_map != null && m_mapRoute != null) {
+                m_map.removeMapObject(m_mapRoute);
+                m_mapRoute = null;
+            } else {
+                /*
+                 * The route calculation requires local map data.Unless there is pre-downloaded
+                 * map data on device by utilizing MapLoader APIs, it's not recommended to
+                 * trigger the route calculation immediately after the MapEngine is
+                 * initialized.The INSUFFICIENT_MAP_DATA error code may be returned by
+                 * CoreRouter in this case.
+                 *
+                 */
+                createRoute();
+            }
+        });
+
+    }
+
+    /* Creates a route from 4350 Still Creek Dr to Langley BC with highways disallowed */
+    private void createRoute() {
+        /* Initialize a CoreRouter */
+        CoreRouter coreRouter = new CoreRouter();
+
+        /* Initialize a RoutePlan */
+        RoutePlan routePlan = new RoutePlan();
+
+        /*
+         * Initialize a RouteOption.HERE SDK allow users to define their own parameters for the
+         * route calculation,including transport modes,route types and route restrictions etc.Please
+         * refer to API doc for full list of APIs
+         */
+        RouteOptions routeOptions = new RouteOptions();
+        /* Other transport modes are also available e.g Pedestrian */
+        routeOptions.setTransportMode(RouteOptions.TransportMode.CAR);
+        /* Disable highway in this route. */
+        routeOptions.setHighwaysAllowed(false);
+        /* Calculate the shortest route available. */
+        routeOptions.setRouteType(RouteOptions.Type.SHORTEST);
+        /* Calculate 1 route. */
+        routeOptions.setRouteCount(1);
+        /* Finally set the route option */
+        routePlan.setRouteOptions(routeOptions);
+
+        /* Define waypoints for the route */
+        /* START: 4350 Still Creek Dr */
+        RouteWaypoint startPoint = new RouteWaypoint(new GeoCoordinate(49.259149, -123.008555));
+        /* END: Langley BC */
+        RouteWaypoint destination = new RouteWaypoint(new GeoCoordinate(49.073640, -122.559549));
+
+        /* Add both waypoints to the route plan */
+        routePlan.addWaypoint(startPoint);
+        routePlan.addWaypoint(destination);
+
+        /* Trigger the route calculation,results will be called back via the listener */
+        coreRouter.calculateRoute(routePlan,
+                new Router.Listener<List<RouteResult>, RoutingError>() {
+                    @Override
+                    public void onProgress(int i) {
+                        /* The calculation progress can be retrieved in this callback. */
+                    }
+
+                    @Override
+                    public void onCalculateRouteFinished(List<RouteResult> routeResults,
+                                                         RoutingError routingError) {
+                        /* Calculation is done. Let's handle the result */
+                        if (routingError == RoutingError.NONE) {
+                            if (routeResults.get(0).getRoute() != null) {
+                                /* Create a MapRoute so that it can be placed on the map */
+                                m_mapRoute = new MapRoute(routeResults.get(0).getRoute());
+
+                                /* Show the maneuver number on top of the route */
+                                m_mapRoute.setManeuverNumberVisible(true);
+
+                                /* Add the MapRoute to the map */
+                                m_map.addMapObject(m_mapRoute);
+
+                                /*
+                                 * We may also want to make sure the map view is orientated properly
+                                 * so the entire route can be easily seen.
+                                 */
+                                GeoBoundingBox gbb = routeResults.get(0).getRoute()
+                                        .getBoundingBox();
+                                m_map.zoomTo(gbb, Map.Animation.NONE,
+                                        Map.MOVE_PRESERVE_ORIENTATION);
+                            } else {
+                                Toast.makeText(m_activity,
+                                        "Error:route results returned is not valid",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(m_activity,
+                                    "Error:route calculation returned error code: " + routingError,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
