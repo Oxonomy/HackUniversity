b69908ac19cc241f21972ff5be4a5ba2c7772096
pv <pv@orbisystems.ru>
2019-03-24 10:37:35 +0300

    Добавленно InfoActivity

--- app/src/main/java/org/neweralosguys/hackuniversity/Product.java
+++ app/src/main/java/org/neweralosguys/hackuniversity/Product.java
@@ -1,17 +1,21 @@
 package org.neweralosguys.hackuniversity;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 public class Product {
+
     public  String Name;
+
     public  Map<String,String> Price;
+
     public  String Calorie;
-    public  float Rating;
+
+    public  Float Rating;
+
     public  String Composition;
 
 
-    public Product(String name, Map<String,String> price, String calorie, float rating, String composition){
+    public Product(String name, Map<String,String> price, String calorie, Float rating, String composition){
         Name = name;
         Price = price;
         Calorie = calorie;
