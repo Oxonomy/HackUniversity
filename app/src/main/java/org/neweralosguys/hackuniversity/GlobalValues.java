e828875a978fc664a90586b256e1de1eec8d0fec
KirillRib <kirillribkin1998@gmail.com>
2019-03-24 09:36:15 +0300

    Добавлен глобальный AllProducts

--- app/src/main/java/org/neweralosguys/hackuniversity/GlobalValues.java
+++ app/src/main/java/org/neweralosguys/hackuniversity/GlobalValues.java
@@ -1,7 +1,10 @@
 package org.neweralosguys.hackuniversity;
 
+import java.util.ArrayList;
+
 public class GlobalValues {
     public static Product currentProduct = null;
     public static String currentStore = "";
 
+    public static ArrayList<Product> AllProducts = new ArrayList();
 }
