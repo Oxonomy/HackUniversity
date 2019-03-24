42844bfcc58dd5831fa44bc566e9f09f2f876a6e
KirillRib <kirillribkin1998@gmail.com>
2019-03-24 11:10:27 +0300

    Сумма товара

--- app/src/main/java/org/neweralosguys/hackuniversity/ProductBasketActivity.java
+++ app/src/main/java/org/neweralosguys/hackuniversity/ProductBasketActivity.java
@@ -59,14 +59,20 @@ public class ProductBasketActivity extends AppCompatActivity {
                 shops.put(s, shops.get(s)+1);
         }
         int maxBasket = 0;
-
-        //for (String s: shops.keySet() )
-
+        for (String s: shops.keySet() )
+            if (maxBasket < shops.get(s)){
+                maxBasket = shops.get(s);
+                GlobalValues.currentStore = s;
+            }
+        float sum = 0;
+        for (Product p: GlobalValues.BasketProducts){
+            sum += Float.parseFloat(p.Price.get(GlobalValues.currentStore));
+        }
 
 
         names = new ArrayList();
         for (Product p: GlobalValues.BasketProducts)
-            names.add(p.Name);
+            names.add(p.Name + " \t " + p.Price.get(GlobalValues.currentStore));
         // создаем адаптер
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, names);
